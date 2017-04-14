edfc2ae16b6eb18cc0a39f9a5f343f5390ff2eae
Miguel Alfonso Bustamante Sanchez <miguel_bustamante_84@outlook.com>
2017-04-13 20:57:18 -0500

    Add project files.

---
+++ AseinfoCalendar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AseinfoCalendar.Startup))]
+namespace AseinfoCalendar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
